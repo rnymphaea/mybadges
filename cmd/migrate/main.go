518eae08813d20e3d20369506d295aed3b71fc81
Lvov Alexandr <strangeisdoctor@gmail.com>
2024-10-13 21:56:45 +0300

    Configuration files added

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -6,12 +6,20 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	"mybadges/internal/config"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	databaseURL := config.GetDatabaseURL(cfg)
 	m, err := migrate.New(
 		"file://internal/database/migrations",
-		"postgres://postgres:psql@localhost:5432/mybadges?sslmode=disable")
+		databaseURL)
 	if err != nil {
 		log.Fatal(err)
 	}
