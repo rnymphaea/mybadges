c9ea845bf4e2706752f13cc12ddeaf57423bc48d
Lvov Alexandr <strangeisdoctor@gmail.com>
2024-10-19 02:14:55 +0300

    deleted tmp file

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ func main() {
 	log.Println("Config loaded: ", cfg)
 	s3cfg := cfg.GetS3Config()
 	s3storage := s3.New(s3cfg.AccessKey, s3cfg.SecretKey, s3cfg.Endpoint, s3cfg.Bucket, s3cfg.Region)
-	
+
 	databaseURL := cfg.GetDatabaseURL()
 	storage, err := postgres.New(databaseURL)
 	if err != nil {
